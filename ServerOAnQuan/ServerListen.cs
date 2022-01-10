151a1177ec786260efc79b43fa75123ff6aca7d9
caotu0903 <62198989+caotu0903@users.noreply.github.com>
2022-01-11 01:11:23 +0700

    Add files via upload
    Update các chức năng về:
    - Tài khoản
    - Xem bảng xếp hạng
    - Phòng chờ
    - Giao tiếp giữa các client khi chơi game

--- ServerOAnQuan/ServerListen.cs
+++ ServerOAnQuan/ServerListen.cs
@@ -79,14 +79,14 @@ namespace ServerOAnQuan
             }
         }
 
-        public static void AddClient(TcpClient tcpClient, string username)
+        public static void AddClient(TcpClient tcpClient, string accountname)
         {
-            htClient.Add(username, tcpClient);
+            htClient.Add(accountname, tcpClient);
         }
 
-        public static void DeleteClient(TcpClient tcpClient, string username)
+        public static void DeleteClient(TcpClient tcpClient, string accountname)
         {
-            htClient.Remove(username);
+            htClient.Remove(accountname);
         }
 
         public static string EncryptPass(string password)
@@ -174,6 +174,14 @@ namespace ServerOAnQuan
                             {
                                 Logout();
                             }
+                            else if (_receiveData.StartsWith("103"))
+                            {
+                                ChangeAccountName();
+                            }
+                            else if (_receiveData.StartsWith("104"))
+                            {
+                                ChangePassword();
+                            }
                             else if (_receiveData.StartsWith("200"))
                             {
                                 CreateRoom();
@@ -202,6 +210,10 @@ namespace ServerOAnQuan
                             {
                                 Unready();
                             }
+                            else if (_receiveData.StartsWith("303"))
+                            {
+                                KickPlayer();
+                            }
                             else if (_receiveData.StartsWith("310"))
                             {
                                 GetRoomInfo();
@@ -210,9 +222,13 @@ namespace ServerOAnQuan
                             {
                                 SendMove();
                             }
+                            else if (_receiveData.StartsWith("402"))
+                            {
+                                SendResult();
+                            }
                             else if (_receiveData.StartsWith("500"))
                             {
-                                Test();
+                                GetRankList();
                             }
                         }
                         else
@@ -221,7 +237,7 @@ namespace ServerOAnQuan
                             {
                                 _login = false;
                                 UpdateStatus(_username, 0);
-                                DeleteClient(_client, _username);
+                                DeleteClient(_client, _accountname);
                                 break;
                             }
                             else
@@ -237,7 +253,7 @@ namespace ServerOAnQuan
                     {
                         _login = false;
                         UpdateStatus(_username, 0);
-                        DeleteClient(_client, _username);
+                        DeleteClient(_client, _accountname);
                     }
                 }
             }
@@ -253,19 +269,29 @@ namespace ServerOAnQuan
                 {
                     SendEncryptMessage("002", _sw); // User đã tồn tại
                 }
-                else {
-                    _sql = "set dateformat DMY ";
-                    _sql += "insert into THONGTINTAIKHOAN (USERNAME, ACCOUNTNAME, EMAIL, PASSWORD, STATUS, CREATETIME) values (";
-                    _sql += "'" + splitData[0] + "','" + splitData[1] + "','" + splitData[2] + "','" + splitData[3] + "', 0, getdate())";
-                    _sqlCommand.CommandText = _sql;
-                    int numberRowAffected = _sqlCommand.ExecuteNonQuery();
-                    if (numberRowAffected == 1)
+                else 
+                {
+                    _sql = "select ACCOUNTNAME from THONGTINTAIKHOAN where ACCOUNTNAME = '" + splitData[1] + "'";
+                    DoSQLSelectCommand();
+                    if (_dataTable.Rows.Count != 0)
                     {
-                        SendEncryptMessage("001", _sw); // Tạo User thành công
+                        SendEncryptMessage("003", _sw); // Tên hiển thị đã tồn tại 
                     }
                     else
                     {
-                        SendEncryptMessage("000", _sw); // Tạo User thất bại
+                        _sql = "set dateformat DMY ";
+                        _sql += "insert into THONGTINTAIKHOAN (USERNAME, ACCOUNTNAME, EMAIL, PASSWORD, STATUS, CREATETIME) values (";
+                        _sql += "'" + splitData[0] + "','" + splitData[1] + "','" + splitData[2] + "','" + splitData[3] + "', 0, getdate())";
+                        _sqlCommand.CommandText = _sql;
+                        int numberRowAffected = _sqlCommand.ExecuteNonQuery();
+                        if (numberRowAffected == 1)
+                        {
+                            SendEncryptMessage("001", _sw); // Tạo User thành công
+                        }
+                        else
+                        {
+                            SendEncryptMessage("000", _sw); // Tạo User thất bại
+                        }
                     }
                 }
             }
@@ -294,7 +320,7 @@ namespace ServerOAnQuan
                             DoSQLSelectCommand();
                             _accountname = _dataTable.Rows[0].ItemArray[0].ToString();
 
-                            AddClient(_client, splitData[0]);
+                            AddClient(_client, _accountname);
                             UpdateStatus(splitData[0], 1);
                             _login = true;
                             _username = splitData[0];
@@ -310,6 +336,50 @@ namespace ServerOAnQuan
                 else if (_row == 0)
                 {
                     SendEncryptMessage("000", _sw); // Đăng nhập thất bại
+                }
+            }
+
+            public void ChangeAccountName()
+            {
+                _receiveData = _receiveData.Remove(0, 3);
+                string[] stringRemove = { "/**/" };
+                string[] splitData = _receiveData.Split(stringRemove, StringSplitOptions.RemoveEmptyEntries);
+                _sql = "select ACCOUNTNAME from THONGTINTAIKHOAN where ACCOUNTNAME = '" + splitData[0] + "'";
+                DoSQLSelectCommand();
+                if (_dataTable.Rows.Count != 0)
+                {
+                    SendEncryptMessage("103" + "003", _sw); // Tên hiển thị đã tồn tại 
+                }
+                else
+                {
+                    _sql = "update THONGTINTAIKHOAN set ACCOUNTNAME = '" + splitData[0] + "' where USERNAME = '" + _username + "'";
+                    _sqlCommand.CommandText = _sql;
+                    _sqlCommand.ExecuteNonQuery();
+                    DeleteClient(_client, _accountname);
+                    _accountname = splitData[0];
+                    AddClient(_client, _accountname);
+                    SendEncryptMessage("103" + "001", _sw);
+                }
+            }
+
+            public void ChangePassword()
+            {
+                _receiveData = _receiveData.Remove(0, 3);
+                string[] stringRemove = { "/**/" };
+                string[] splitData = _receiveData.Split(stringRemove, StringSplitOptions.RemoveEmptyEntries);
+                _sql = "select PASSWORD from THONGTINTAIKHOAN where USERNAME = '" + _username + "'";
+                DoSQLSelectCommand();
+                string PassInDatabase = _dataTable.Rows[0].ItemArray[0].ToString();
+                if (splitData[0] == PassInDatabase)
+                {
+                    _sql = "update THONGTINTAIKHOAN set PASSWORD = '" + splitData[1] + "' where USERNAME = '" + _username + "'";
+                    _sqlCommand.CommandText = _sql;
+                    _sqlCommand.ExecuteNonQuery();
+                    SendEncryptMessage("104" + "001", _sw); // Đổi mật khẩu thành công
+                }
+                else
+                {
+                    SendEncryptMessage("104" + "000", _sw); // Mật khẩu hiện tại sai
                 }
             }
 
@@ -345,7 +415,7 @@ namespace ServerOAnQuan
                     }
                 }
                 ArrayList arrayList = new ArrayList();
-                arrayList.Add(_username);
+                arrayList.Add(_accountname);
                 htGameRoom.Add("OAQ-" + i, arrayList);
 
                 SendEncryptMessage("001" + "OAQ-" + i, _sw); // Tạo phòng thành công
@@ -361,7 +431,7 @@ namespace ServerOAnQuan
                     if (GetNumberPlayerInRoom(splitData[0]) == 1)
                     {
                         ArrayList arrayList = (ArrayList)htGameRoom[splitData[0]];
-                        arrayList.Add(_username);
+                        arrayList.Add(_accountname);
                         SendEncryptMessage("210", _sw); // Vào phòng thành công
                         TcpClient tcpClient = (TcpClient)htClient[arrayList[0]];
                         StreamWriter sw = new StreamWriter(tcpClient.GetStream());
@@ -395,12 +465,12 @@ namespace ServerOAnQuan
                 ArrayList arrayList = (ArrayList)htGameRoom[splitData[0]];
                 if (arrayList.Count == 1)
                 {
-                    arrayList.Remove(_username);
+                    arrayList.Remove(_accountname);
                     htGameRoom.Remove(splitData[0]);
                 }
                 else if (arrayList.Count == 2) {
 
-                    arrayList.Remove(_username);
+                    arrayList.Remove(_accountname);
 
                     TcpClient tcpClient = (TcpClient)htClient[arrayList[0]];
                     StreamWriter sw = new StreamWriter(tcpClient.GetStream());
@@ -414,6 +484,34 @@ namespace ServerOAnQuan
                 SendEncryptMessage("001", _sw);
             }
 
+            public void KickPlayer()
+            {
+                _receiveData = _receiveData.Remove(0, 3);
+                string[] stringRemove = { "/**/" };
+                string[] splitData = _receiveData.Split(stringRemove, StringSplitOptions.RemoveEmptyEntries);
+
+                string getRoomInfoMessage = "310";
+
+                ArrayList arrayList = (ArrayList)htGameRoom[splitData[0]];
+
+                try
+                {
+                    TcpClient tcpClient = (TcpClient)htClient[arrayList[1]];
+                    StreamWriter sw = new StreamWriter(tcpClient.GetStream());
+
+                    SendEncryptMessage("303", sw); // Gửi thông tin kick tới player 2 
+
+                    arrayList.Remove(arrayList[1]);
+                }
+                catch
+                {
+                    
+                }
+
+                getRoomInfoMessage += splitData[0] + "/**/" + arrayList[0] + "/**/" + "unknown" + "/**/" + "false/**/";
+                SendEncryptMessage(getRoomInfoMessage, _sw); // Gửi thông tin kick thành công tới host
+            }
+
             public void SendMove()
             {
                 _receiveData = _receiveData.Remove(0, 3);
@@ -422,7 +520,7 @@ namespace ServerOAnQuan
                 ArrayList arrayList = (ArrayList)htGameRoom[splitData[0]];
                 foreach (string user in arrayList)
                 {
-                    if (user == _username)
+                    if (user == _accountname)
                     {
                         string moveMessage = "400" + splitData[1] + "/**/" + splitData[2] + "/**/" + splitData[3] + "/**/";
                         SendEncryptMessage(moveMessage, _sw);
@@ -447,6 +545,95 @@ namespace ServerOAnQuan
                 ArrayList arrayList = (ArrayList)htGameRoom[splitData[0]];
 
                 SendToAllPlayerInRoom(splitData[0], "300");
+            }
+
+            public void FinishGame()
+            {
+                _receiveData = _receiveData.Remove(0, 3);
+                string[] stringRemove = { "/**/" };
+                string[] splitData = _receiveData.Split(stringRemove, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            public void GetRankList()
+            {
+                string getRankInfoMessage = "500";
+
+                _sql = "select dense_rank() over (order by POINT desc) as [RANK], ACCOUNTNAME ,POINT ";
+                _sql += "from BANGXEPHANG BXH join THONGTINTAIKHOAN TTTK on BXH.USERNAME = TTTK.USERNAME ";
+                _sql += "where GAMECODE = 'OAQ'";
+                DoSQLSelectCommand();
+
+                if (_dataTable.Rows.Count != 0)
+                {
+                    for (int i = 0; i < _dataTable.Rows.Count; i++)
+                    {
+                        getRankInfoMessage += _dataTable.Rows[i].ItemArray[0] + "/**/" + _dataTable.Rows[i].ItemArray[1] + "/**/" + _dataTable.Rows[i].ItemArray[2] + "/**/";
+                    }
+                    _frmServerListen.InfoMessage(getRankInfoMessage + "\r\n");
+                    SendEncryptMessage(getRankInfoMessage, _sw);
+                }
+                else
+                {
+                    _frmServerListen.InfoMessage(getRankInfoMessage + "\r\n");
+                    SendEncryptMessage(getRankInfoMessage, _sw);
+                }
+            }
+
+            public void SendResult()
+            {
+                _receiveData = _receiveData.Remove(0, 3);
+                string[] stringRemove = { "/**/" };
+                string[] splitData = _receiveData.Split(stringRemove, StringSplitOptions.RemoveEmptyEntries);
+
+                _sql = "select POINT from BANGXEPHANG where USERNAME = '" + _username + "' and GAMECODE = '" + splitData[0] + "'"; 
+                DoSQLSelectCommand();
+
+                int point = 0;
+
+                if (splitData[1] == "win")
+                {
+                    point = 10;
+                }
+                else if (splitData[1] == "lose")
+                {
+                    point = -10;
+                }
+
+                if (_dataTable.Rows.Count == 0)
+                {
+                    if (point >= 0)
+                    {
+                        _sql = "insert into BANGXEPHANG (USERNAME, GAMECODE, POINT) values (";
+                        _sql += "'" + _username + "','" + splitData[0] + "'," + point + ")";
+                        _sqlCommand.CommandText = _sql;
+                        _sqlCommand.ExecuteNonQuery();
+                    }
+                    else
+                    {
+                        _sql = "insert into BANGXEPHANG (USERNAME, GAMECODE, POINT) values (";
+                        _sql += "'" + _username + "','" + splitData[0] + "'," + 0 + ")";
+                        _sqlCommand.CommandText = _sql;
+                        _sqlCommand.ExecuteNonQuery();
+                    }
+                }
+                else
+                {
+                    if ((int)_dataTable.Rows[0].ItemArray[0] >= 10)
+                    {
+                        _sql = "update BANGXEPHANG set POINT =  POINT + " + point + " where USERNAME = '" + _username + "' and GAMECODE = '" + splitData[0] + "'";
+                        _sqlCommand.CommandText = _sql;
+                        _sqlCommand.ExecuteNonQuery();
+                    }
+                    else
+                    {
+                        if (point == 10)
+                        {
+                            _sql = "update BANGXEPHANG set POINT =  POINT + " + point + " where USERNAME = '" + _username + "' and GAMECODE = '" + splitData[0] + "'";
+                            _sqlCommand.CommandText = _sql;
+                            _sqlCommand.ExecuteNonQuery();
+                        }
+                    }
+                }
             }
 
             public void Ready()
@@ -478,7 +665,7 @@ namespace ServerOAnQuan
                 SendEncryptMessage("102", _sw);
                 _login = false;
                 UpdateStatus(_username, 0);
-                DeleteClient(_client, _username);
+                DeleteClient(_client, _accountname);
             }
 
             public void UpdateStatus(string username, int status)
@@ -552,11 +739,6 @@ namespace ServerOAnQuan
                     StreamWriter sw = new StreamWriter(tcpClient.GetStream());
                     SendEncryptMessage(message, sw);
                 }
-            }
-
-            public void Test()
-            {
-                SendEncryptMessage("501", _sw);
             }
         }
     }
